c35ad0496590efb064853d1492ae458bcd208968
Agung Sidharta So <agung_sidharta_gungz@yahoo.com>
2023-02-25 19:00:44 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,18 +5,24 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const _ = require('lodash');
+const csrf = require('csurf');
+const csrfProtection = csrf();
 
 const options = {};
 const flag = fs.readFileSync('./flag', 'utf-8').trim();
 const docHtml = fs.readFileSync('./src/index.html', 'utf-8');
 
 app.use(bodyParser.json());
+app.use(function(req, res, next){
+  res.removeHeader("X-Powered-By");
+  next();
+});
 
 app.get('/', (req, res) => {
     res.send(docHtml);
 });
 
-app.post('/echo', (req, res) => {
+app.post('/echo', csrfProtection, (req, res) => {
     const out = {
         userID: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
         time: Date.now()
